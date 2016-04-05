7322cc6852f00ae2c06397d47f371f3d06f49b13
Tahmid Mahmud <tmahmud94@gmail.com>
2016-04-05 02:13:12 -0500

    commit -m "added scheduling page"

--- code/employeeList.php
+++ code/employeeList.php
@@ -24,62 +24,63 @@ $siteBuilder->getMenu();
 ?>
 
 <div class = "content" >
-            <h1>Employee List</h1>
-            <div class="reports">
-                <?php
-                    require_once('../db_connection.php');
+    <h1>Employee List</h1>
+    <div class="reports">
+        <?php
+            require_once('../db_connection.php');
 
-                    $query = "SELECT idUsers, Roles.name, first_name, last_name, email, address, phone,
-                    ssn, gender, date_employed FROM Users, Roles Where idRoles = role_id";
+            $query = "SELECT idUsers, Roles.name, first_name, last_name, email, address, phone,
+            ssn, gender, date_employed FROM Users, Roles Where idRoles = role_id";
 
-                    $response = @mysqli_query($dbc, $query);
+            $response = @mysqli_query($dbc, $query);
 
-                    if($response){
-                    echo '<table align="left"
-                    cellspacing="5" cellpadding="8" class="report">
+            if($response){
+            echo '<table align="left"
+            cellspacing="5" cellpadding="8" class="report">
 
-                    <tr><td align="left"><b>ID</b></td>
-                    <td align="left"><b>Role</b></td>
-                    <td align="left"><b>First Name</b></td>
-                    <td align="left"><b>Last Name</b></td>
-                    <td align="left"><b>Email</b></td>
-                    <td align="left"><b>Address</b></td>
-                    <td align="left"><b>Phone</b></td>
-                    <td align="left"><b>SSN</b></td>
-                    <td align="left"><b>Gender</b></td>
-                    <td align="left"><b>Date Employed</b></td></tr>';
+            <tr><td align="left"><b>ID</b></td>
+            <td align="left"><b>Role</b></td>
+            <td align="left"><b>First Name</b></td>
+            <td align="left"><b>Last Name</b></td>
+            <td align="left"><b>Email</b></td>
+            <td align="left"><b>Address</b></td>
+            <td align="left"><b>Phone</b></td>
+            <td align="left"><b>SSN</b></td>
+            <td align="left"><b>Gender</b></td>
+            <td align="left"><b>Date Employed</b></td></tr>';
 
-                
-                    while($row = mysqli_fetch_array($response)){
+        
+            while($row = mysqli_fetch_array($response)){
 
-                    echo '<tr><td align="left">' . 
-                    $row['idUsers'] . '</td><td align="left">' . 
-                    $row['name'] . '</td><td align="left">' .
-                    $row['first_name'] . '</td><td align="left">' . 
-                    $row['last_name'] . '</td><td align="left">' .
-                    $row['email'] . '</td><td align="left">' . 
-                    $row['address'] . '</td><td align="left">' . 
-                    $row['phone'] . '</td><td align="left">' .
-                    $row['ssn'] . '</td><td align="left">' .
-                    $row['gender'] . '</td><td align="left">' .
-                    $row['date_employed'] . '</td><td align="left">';
+            echo '<tr><td align="left">' . 
+            $row['idUsers'] . '</td><td align="left">' . 
+            $row['name'] . '</td><td align="left">' .
+            $row['first_name'] . '</td><td align="left">' . 
+            $row['last_name'] . '</td><td align="left">' .
+            $row['email'] . '</td><td align="left">' . 
+            $row['address'] . '</td><td align="left">' . 
+            $row['phone'] . '</td><td align="left">' .
+            $row['ssn'] . '</td><td align="left">' .
+            $row['gender'] . '</td><td align="left">' .
+            $row['date_employed'] . '</td><td align="left">';
 
-                    echo '</tr>';
-                    }
+            echo '</tr>';
+            }
 
-                    echo '</table>';
-                    } else {
+            echo '</table>';
+            } else {
 
-                    echo "Couldn't issue database query<br />";
+            echo "Couldn't issue database query<br />";
 
-                    echo mysqli_error($dbc);
+            echo mysqli_error($dbc);
 
-                    }
+            }
 
-                    mysqli_close($dbc);
+            mysqli_close($dbc);
 
-                    ?>
-            </div>
+            ?>
+    </div>
+</div>
 
 <?php
 $siteBuilder->getClosinghtmlTags();
--- code/scheduling.php
+++ code/scheduling.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+$_SESSION['week'] = ((isset($_SESSION['week'])) ? $_SESSION['week'] : 0);
+if(isset($_GET['Next'])){
+     $_SESSION['week']++;
+}
+if(isset($_GET['Previous'])){
+     $_SESSION['week']--;
+}
+
 if(isset($_SESSION['id'])){
     $username = $_SESSION['username']; 
     $id = $_SESSION['id'];
@@ -24,10 +32,86 @@ $siteBuilder->getMenu();
 ?>
 
 <div class = "content" >
-    <center class="info">
-        <h1>Scheduling</h1>
-        <p>Sorry not implemented</p>
-    </center>
+    <h1>Shift Schedule</h1>
+    <form action="scheduling.php" method="get">
+        <input type="submit" name="Previous" value="Previous" />
+        <input type="submit" name="Next" value="Next" />
+    </form>
+
+
+    
+        <?php
+                $week = 0;
+                
+                if(isset($_POST['week'])){
+                    if($_POST['week'] == 'Next'){
+                        $week = $week + 1;        
+                    }
+                    if($_POST['week'] == 'Previous'){
+                        
+                    }
+                }
+            
+                echo '<div class="reports">';
+
+                require_once('../db_connection.php');
+
+               
+
+                $query = "SELECT 
+                            shift.idShift_Schedule, shift.shift_begin, shift.shift_end,
+                            (SELECT idUsers FROM Users user WHERE idUsers=". $id ." AND user.idUsers = shift.worker_id) AS workerID,
+                            (SELECT first_name FROM Users user WHERE idUsers=". $id ." AND user.idUsers = shift.worker_id) AS workerFirstName,
+                            (SELECT last_name FROM Users user WHERE idUsers=". $id ." AND user.idUsers = shift.worker_id) AS workerLastName,
+                            (SELECT idUsers FROM Users user WHERE user.idUsers = shift.created_by) AS managerID,
+                            (SELECT first_name FROM Users user WHERE user.idUsers = shift.created_by) AS managerFirstName,
+                            (SELECT last_name FROM Users user WHERE user.idUsers = shift.created_by) AS managerLastName,
+                            (SELECT phone FROM Users user WHERE user.idUsers = shift.created_by) AS managerPhone
+                            FROM Shift_Schedule shift WHERE YEARWEEK(shift.shift_begin) = (YEARWEEK(NOW()) + " . $_SESSION['week'] . ")";
+
+                $response = @mysqli_query($dbc, $query);
+                
+                if($response){
+                echo '<table align="left"
+                cellspacing="5" cellpadding="8" class="report">
+
+                <tr><td align="left"><b>ID</b></td>
+                <td align="left"><b>Shift Begin</b></td>
+                <td align="left"><b>Shift End</b></td>
+                <td align="left"><b>First Name</b></td>
+                <td align="left"><b>Last Name</b></td>
+                <td align="left"><b>Manager</b></td>
+                <td align="left"><b>Phone</b></td></tr>';
+
+            
+                while($row = mysqli_fetch_array($response)){
+
+                echo '<tr><td align="left">' . 
+                $row['workerID'] . '</td><td align="left">' . 
+                $row['shift_begin'] . '</td><td align="left">' .
+                $row['shift_end'] . '</td><td align="left">' . 
+                $row['workerFirstName'] . '</td><td align="left">' .
+                $row['workerLastName'] . '</td><td align="left">' . 
+                $row['managerFirstName'] . ' ' . $row['managerLastName'] . '</td><td align="left">' . 
+                $row['managerPhone'] . '</td><td align="left">';
+
+                echo '</tr>';
+                }
+
+                echo '</table>';
+                } else {
+
+                echo "Couldn't issue database query<br />";
+
+                echo mysqli_error($dbc);
+
+                }
+
+                mysqli_close($dbc);
+
+                echo '</div>';
+        ?>
+  
 </div>
 
 <?php
