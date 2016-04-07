86131083bf674a9443449a07351b6f32a75bd121
Nixon Mathew <nmathew33@gmail.com>
2016-04-06 20:20:15 -0500

    Added forms
    Get number of adults, children, and infants

--- code/ticketing.php
+++ code/ticketing.php
@@ -25,11 +25,33 @@ $siteBuilder->getMenu();
 
 <div class = "content" >
     <center class="info">
-        <h1>Ticketing</h1>
-        <p>Sorry not implemented</p>
+        <h1>Ticketing</h1>    
     </center>
+	
+	<div class = "count">
+		<h2>Log In</h2>
+			<form action="ticketing.php" method="post"> 
+				<div class = "num_adults">
+					<h3>Number of Adults</h3>
+					<input type="number" name="adults" min=1>
+				</div>
+				<div class = "num_children">
+					<h3>Number of Children (Under "48)</h3> 
+					<input type="number" name="children" min=0>
+				</div>
+				<div class = "num_infants">
+					<h3>Number of Infants (Under 2 years)</h3> 
+					<input type="number" name="infants" min=0>
+				</div>
+			    <input type="submit" name="submit" value="Continue" class ="button"> 
+			</form>	
+	</div>
 </div>
 
 <?php
+$adults = $_POST['adults'];
+$children = $_POST['children'];
+$infants = $_POST['infants'];
+
 $siteBuilder->getClosinghtmlTags();
 ?>
