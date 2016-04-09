1fd1b397430368cf42e8c05afafb8fbd1f7d0899
Tahmid Mahmud <tmahmud94@gmail.com>
2016-04-09 16:59:56 -0500

    reverted back to daniels version

--- code/ticketing.php
+++ code/ticketing.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 if(isset($_SESSION['id'])){
     $username = $_SESSION['username']; 
     $id = $_SESSION['id'];
@@ -10,29 +9,19 @@ if(isset($_SESSION['id'])){
 } else{
     header("Location: index.php");
 }
-
-
 // $query = "INSERT INTO Users (idUsers, role_id, first_name, last_name, email,
 // address, phone, ssn, gender, password, date_employed) VALUES ( ?, ?, ?, ?,
 // ?, ?, ?, ?, ?, ?, ?)";
-
 require("themeparkSiteBuilder.php");
 $siteBuilder = new themeParkSiteBuilder();
-
 $siteBuilder->getOpeningHtmlTags('Ticketing');
-
 $siteBuilder->getGreyOverLay();
-
 $siteBuilder->getSubTitle();
-
 $siteBuilder->getMenu();
 ?>
 
 <div class = "content" >
     <center class="info">
-<<<<<<< HEAD
-        <h1>Ticketing</h1>    
-=======
         <h1>Ticketing</h1>
         <form action="form_sent.php" method="post">
             <div id="adultList">
@@ -83,33 +72,9 @@ $siteBuilder->getMenu();
             
             <input type="submit">
         </form>
->>>>>>> refs/remotes/tahmidMahmud/master
     </center>
-	
-	<div class = "count">
-		<h2>Log In</h2>
-			<form action="ticketing.php" method="post"> 
-				<div class = "num_adults">
-					<h3>Number of Adults</h3>
-					<input type="number" name="adults" min=1>
-				</div>
-				<div class = "num_children">
-					<h3>Number of Children (Under "48)</h3> 
-					<input type="number" name="children" min=0>
-				</div>
-				<div class = "num_infants">
-					<h3>Number of Infants (Under 2 years)</h3> 
-					<input type="number" name="infants" min=0>
-				</div>
-			    <input type="submit" name="submit" value="Continue" class ="button"> 
-			</form>	
-	</div>
 </div>
 
 <?php
-$adults = $_POST['adults'];
-$children = $_POST['children'];
-$infants = $_POST['infants'];
-
 $siteBuilder->getClosinghtmlTags();
 ?>
