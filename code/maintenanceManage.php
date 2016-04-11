2fac2e76e684ba2c86000503445a57ffcdc34a56
saad2139 <saad2139@gmail.com>
2016-04-11 01:13:12 -0500

    Maintenance Functionality

--- code/maintenanceManage.php
+++ code/maintenanceManage.php
@@ -39,17 +39,19 @@ $siteBuilder->getMenu();
             require_once('../db_connection.php');
 
             $query = "SELECT idMaintenance, user_created, date_created, ticket_description, ride
-            FROM Maintenance";
+            FROM Maintenance WHERE user_closed IS NULL";
 
             $response = @mysqli_query($dbc, $query);
+            
+            echo '<form action="UpdateMaintenanceForm.php" method="post" enctype="multipart/form=data">';
+
             
             if($response)
             {
                 echo '<table align = left"
                 cellspacing="10" cellpadding = "10" class="report">
-                
+                <tr><td align = "left"><b> Active Tickets:</b></td></tr>
                 <tr>
-                <td align="left"></td>
                 <td align="left"></td>
                 <td align = "left"><b>Ticket #</b></td>
                 <td align="left"><b>Date_Created</b></td>
@@ -60,20 +62,52 @@ $siteBuilder->getMenu();
                 while($row = mysqli_fetch_array($response))
                 {
                     echo '<tr><td align="left">'.
-                    '<button type="submit" name="updateMaintenance" value="' . $row['idMaintenance'] . '">Modify</button>'  . '</td><td align="left">' . 
-                    '<button type="submit" name="closeMaintenance" value="' . $row['idMaintenance'] . '">Resolve</button>'  . '</td><td align="left">' .
+                    '<button type="submit" name="updateMaintenance" value="' . $row['idMaintenance'] . '">Resolve</button>'  . '</td><td align="left">' . 
                     $row['idMaintenance'] . '</td><td align="left">' . 
                     $row['date_created'] . '</td><td align="left">' .
                     $row['user_created'] . '</td><td align="left">' . 
                     $row['ride'] . '</td><td align="left">' .
                     $row['ticket_description'] . '</td><td align="left">';  
                     echo '</tr>';           
+                }    
+           }
+            $query = "SELECT idMaintenance, user_created, date_created, user_closed,date_closed,
+            ticket_description,closed_description, ride FROM UmaLand.Maintenance WHERE user_closed IS NOT NULL ";
+
+            $response = @mysqli_query($dbc, $query);
+            
+            if($response)
+            {
+                echo
+                '<tr><td align = "left"><b> Closed Tickets:</b></td></tr>
+                <tr>
+                <td align="left"></td>                
+                <td align = "left"><b>Ticket #</b></td>
+                <td align="left"><b>Date_Created</b></td>
+                <td align="left"><b>User_Created</b></td>
+                <td align="left"><b>Ride ID</b></td>
+                <td align="left"><b>Ticket_Description</b></td>
+                <td align="left"><b>Date_Closed</b></td>
+                <td align="left"><b>User_Closed</b></td>
+                <td align="left"><b>Resolution</b></td>';
+                
+                while($row = mysqli_fetch_array($response))
+                {
+                    echo '<tr><td align="left">'.
+                    '<button type="submit" name="updateMaintenance" value="' . $row['idMaintenance'] . '">Archive</button>'  . '</td><td align="left">' .
+                    $row['idMaintenance'] . '</td><td align="left">' . 
+                    $row['date_created'] . '</td><td align="left">' .
+                    $row['user_created'] . '</td><td align="left">' .
+                    $row['ride'] . '</td><td align="left">' .
+                    $row['ticket_description'] . '</td><td align="left">'.
+                    $row['user_closed'] . '</td><td align="left">'.
+                    $row['date_closed'] . '</td><td align="left">'.                          
+                    $row['closed_description'] . '</td><td align="left">'; 
+                    echo '</tr>';             
                 }
-                echo '</table>';
-                //echo '</form>';
-                mysqli_close($dbc);
-                     
-           }     
+                  echo '</table>';  
+             }
+              mysqli_close($dbc);     
             ?>
     </div>
 
