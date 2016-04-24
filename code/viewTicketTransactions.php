0b1a95e6eb8ec249647b61cbb85e63ec5d51cd54
Nixon Mathew <nmathew33@gmail.com>
2016-04-24 00:32:56 -0500

    Added header for Concession Transactions, Ride Usage, and Ticket Transactions

--- code/viewTicketTransactions.php
+++ code/viewTicketTransactions.php
@@ -36,7 +36,17 @@ $siteBuilder->getMenu();
                 $month = $_POST['yearMonth'];
 
                 echo '<div class="reports">';
+                ?>
 
+                <div class="report-header">
+                    <h4>Four O Four Land</h4>
+                    4773 Ashmor Drive <br />
+                    Houston, TX <br />
+
+                <?php
+                date_default_timezone_set('America/Chicago');
+                $date = date('m/d/Y h:i:s a', time());
+                echo "<br />This report was generated at $date</div>";
                 require_once('../db_connection.php');
 
                 $month = $month . '%';
