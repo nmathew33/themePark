c963a76b41428739db4959e9d12c64c8b02123c1
Nixon Mathew <nmathew33@gmail.com>
2016-04-25 15:27:20 -0500

    Fixed same display bug in view Concession

--- code/viewConcession.php
+++ code/viewConcession.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(isset($_SESSION['id'])){
-    $username = $_SESSION['username']; 
+    $username = $_SESSION['username'];
     $id = $_SESSION['id'];
     $roleId = $_SESSION['roleId'];
     $first_name = $_SESSION['first_name'];
@@ -33,32 +33,32 @@ $siteBuilder->getMenu();
     </form>
 
 
-    
+
         <?php
             if(isset($_POST['by'])){
-                
+
             $by = $_POST['by'];
 
-                echo '<form action="editingConcession.php" method="post" enctype="multipart/form=data"> 
+                echo '<form action="editingConcession.php" method="post" enctype="multipart/form=data">
                             <button type="submit" name="by" value="' . $by . '">Select</button>
                         </form> ';
                 echo '<div class="reports">';
-                
+
                 $query = "SELECT idConcession_Pricing, Concession_Pricing.name, Concession_Pricing.price, Concession_Stands.name AS cstn, Concession_Stands.location FROM Concession_Pricing, Concession_Stands WHERE Concession_Pricing.location = idConcession_Stands AND Concession_Pricing.archive='no' ORDER BY " . $by;
             } else{
-                
-                 echo '<form action="editingConcession.php" method="post" enctype="multipart/form=data"> 
+
+                 echo '<form action="editingConcession.php" method="post" enctype="multipart/form=data">
                         <button type="submit" value="">Select</button>
                     </form> ';
                 echo '<div class="reports">';
-            
+
                 $query = "SELECT idConcession_Pricing, Concession_Pricing.name, Concession_Pricing.price, Concession_Stands.name AS cstn, Concession_Stands.location FROM Concession_Pricing, Concession_Stands WHERE Concession_Pricing.location = idConcession_Stands AND Concession_Pricing.archive='no'";
-                
+
             }
             require_once('../db_connection.php');
 
             $response = @mysqli_query($dbc, $query);
-            
+
             if($response){
             echo '<table align="left"
             cellspacing="5" cellpadding="8" class="report">
@@ -69,15 +69,15 @@ $siteBuilder->getMenu();
             <td align="left"><b>Location</b></td>
             <td align="left"><b>Price</b></td></tr>';
 
-        
+
             while($row = mysqli_fetch_array($response)){
 
-            echo '<tr><td align="left">' . 
-            $row['idConcession_Pricing'] . '</td><td align="left">' . 
+            echo '<tr><td align="left">' .
+            $row['idConcession_Pricing'] . '</td><td align="left">' .
             $row['name'] . '</td><td align="left">' .
             $row['cstn'] . '</td><td align="left">' .
             $row['location'] . '</td><td align="left">' .
-            $row['price'] . '</td><td align="left">';
+            $row['price'] . '</td>';
 
             echo '</tr>';
             }
@@ -95,7 +95,7 @@ $siteBuilder->getMenu();
 
             echo '</div>';
         ?>
-  
+
 <?php
 $siteBuilder->getClosinghtmlTags();
 ?>
